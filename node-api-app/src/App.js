13be12b80ec623e2cbc58978532815dd12123d9e
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-28 19:14:11 +0100

    Add navbar component to application

--- node-api-app/src/App.js
+++ node-api-app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello from react</h1>
+      <Navbar />
     </div>
   );
 }
