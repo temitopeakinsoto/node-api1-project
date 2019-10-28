4422d518e26fc782d0f2f95892995983b3880880
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-28 18:32:00 +0100

    Create a status codes for successful and failed api requests

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ app.delete("/api/users/:id", deleteUserById);
 
 function createNewUser(req, res) {
   const newUser = req.body;
-  //newUser = {name, bio}
-
   if (!newUser.name || !newUser.bio) {
     res
       .status(400)
@@ -33,13 +31,23 @@ function createNewUser(req, res) {
 
 function getUserById(req, res) {
   const { id } = req.params;
-
   db.findById(id)
     .then(user => {
-      res.status(200).json(user);
+      if(user){
+        res.status(200).json(user);
+      }else{
+        res
+        .status(404)
+        .json({ message: 'The user with the specified ID does not exist.' });
+      }
+      
     })
-    .catch(error => {
-      res.json(errorMessage, error);
+    .catch(() => {
+      res
+        .status(500)
+        .json({
+          errorMessage: "There was an error with this request."
+        });
     });
 }
 
@@ -50,19 +58,16 @@ function updateUserById(req, res) {
   if (!userDetails.name || !userDetails.bio) {
     res
       .status(400)
-      .json({ errorMessage: 'Please provide name and bio for the user.' });
-  }
-  else {
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
     db.update(id, userDetails)
-    .then(user => {
-      res.status(200).json(user);
-    })
-    .catch(error => {
-      res.json(errorMessage, error);
-    });
+      .then(user => {
+        res.status(200).json(user);
+      })
+      .catch(error => {
+        res.json(errorMessage, error);
+      });
   }
-
-  
 }
 
 function getAllUsers(req, res) {
@@ -72,8 +77,10 @@ function getAllUsers(req, res) {
     })
     .catch(() => {
       res
-      .status(500)
-      .json({ errorMessage: "The users information could not be retrieved."  });
+        .status(500)
+        .json({
+          errorMessage: "The users information could not be retrieved."
+        });
     });
 }
 
@@ -81,11 +88,23 @@ function deleteUserById(req, res) {
   const { id } = req.params;
 
   db.remove(id)
-    .then(data => {
-      res.status(200).json(data);
+    .then(count => {
+      if (count && count > 0) {
+        res.status(200).json({
+          message: "the user was successfully deleted."
+        });
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
     })
-    .catch(error => {
-      res.json(errorMessage, error);
+    .catch(() => {
+      res
+        .status(500)
+        .json({
+          errorMessage: "The users information could not be retrieved."
+        });
     });
 }
 
