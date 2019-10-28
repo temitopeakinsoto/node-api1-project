4ad2da44f49110ca196f018d2043ad4535e46bd4
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-28 15:16:12 +0100

    Create a delete endpoint
    In this commit, I created a delete endpoint for deleting a particular user.
    This API endpoint receives delete request from the client side.
    when a request matches the delete('/api/users/:id') route, a callback handler function called deleteUserById() is triggered.
    this function uses the id that was passed into it to call a remove() helper method available in db.js to query the database and delete any user which matches that particular id

--- index.js
+++ index.js
@@ -4,22 +4,24 @@ const db = require('./data/db.js')
 const app = express()
 app.use(cors()) 
 app.use(express.json()) 
+const errorMessage = "There was an error with this request...";
 
 
 app.get('/api/users', getAllUsers)
 app.get('/api/users/:id', getUserById)
 app.post('/api/users', createNewUser)
 app.put('/api/users/:id', updateUserById)
+app.delete('/api/users/:id', deleteUserById)
 
 function createNewUser(req, res){
     const newUser = req.body;
 
     db.insert(newUser)
     .then(data => {
-        res.json(data);
+        res.status(200).json(data);
     })
     .catch(error => {
-        res.json(error)
+        res.status(errorMessage, error)
     })
 
 }
@@ -29,32 +31,45 @@ function getUserById(req, res) {
     
     db.findById(id)
     .then(data => {
-        res.json(data);
+        res.status(200).json(data);
     })
     .catch(error => {
-        console.log(error);
+        res.json(errorMessage, error);
     })
 }
 
 function updateUserById(req, res){
     const {id} = req.params;
-    const userDetails = req.body
+    const userDetails = req.body;
+
     db.update(id, userDetails)
     .then(data => {
-        res.json(data)
+        res.status(200).json(data)
     })
     .catch(error => {
-        res.json(error)
+        res.json(errorMessage, error)
     })
 }
 
 function getAllUsers(req, res){
     db.find()
     .then(data => {
-      res.json(data)
+        res.status(200).json(data)
     })
     .catch(error => {
-      console.log(error);
+      res.json(errorMessage, error);
+    })
+}
+
+function deleteUserById(req, res) {
+    const {id} = req.params;
+
+    db.remove(id)
+    .then(data => {
+        res.status(200).json(data);
+    })
+    .catch(error => {
+        res.json(errorMessage, error)
     })
 }
 
