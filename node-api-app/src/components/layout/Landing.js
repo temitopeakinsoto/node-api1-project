edf3dbc134815b2bdfd750bd96e569fdccde538b
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-29 07:54:53 +0100

    wip

--- node-api-app/src/components/layout/Landing.js
+++ node-api-app/src/components/layout/Landing.js
@@ -10,8 +10,7 @@ export default function Landing() {
               <h1 className="display-3 mb-4">NODE-API APP</h1>
               <p className="lead">
                 {" "}
-                Create a developer profile/portfolio, share posts and get help
-                from other developers
+                Create a simple application to perform CRUD database operations
               </p>
               <hr />
               <a href="#" className="btn btn-lg btn-info mr-2">
